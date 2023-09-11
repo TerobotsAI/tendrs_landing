62d22d1482143758e8a1bd1a2cb59551882131eb
Gautam Singla <ggsingla777@gmail.com>
2023-09-11 19:53:41 +0530

    Hero section image updated

--- pages/_home.tsx
+++ pages/_home.tsx
@@ -1,14 +1,8 @@
-import About from '@/components/Home/About'
 import CTA from '@/components/Home/CTA'
 import DetailedFeatures from '@/components/Home/DetailedFeatures'
-import Differences from '@/components/Home/Differences'
 import FAQs from '@/components/Home/FAQs'
 import Features from '@/components/Home/Features'
 import Hero from '@/components/Home/Hero'
-import HowItWorks from '@/components/Home/HowItWorks'
-import LogoCloud from '@/components/Home/LogoCloud'
-import Stats from '@/components/Home/Stats'
-import WaitlistForm from '@/components/Home/WaitlistForm'
 import Footer from '@/components/common/Footer'
 import Navbar from '@/components/common/Navbar'
 import React from 'react'
@@ -18,14 +12,8 @@ const HomePage = () => {
         <>
             <Navbar />
             <Hero />
-            {/* <LogoCloud /> */}
-            {/* <WaitlistForm /> */}
-            {/* <About /> */}
-            {/* <HowItWorks /> */}
-            {/* <Differences /> */}
             <Features />
             <DetailedFeatures />
-            {/* <Stats /> */}
             <CTA />
             <FAQs />
             <Footer />
